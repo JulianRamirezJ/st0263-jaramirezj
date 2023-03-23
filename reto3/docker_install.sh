0ba7c282885c573687aac7120acfd20d0e6af5aa
= <57159295+JulianRamirezJ@users.noreply.github.com>
2023-03-23 18:07:08 -0500

    Boot for nfs client added

--- reto3/docker_install.sh
+++ reto3/docker_install.sh
@@ -2,6 +2,5 @@ sudo apt update
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo apt install git -y
-
 sudo systemctl enable docker
 sudo systemctl start docker
--- reto3/wordpress_server/nfs_setup.sh
+++ reto3/wordpress_server/nfs_setup.sh
@@ -1,5 +1,8 @@
 sudo apt update
 sudo apt install nfs-common
 sudo mkdir -p /var/www/html
-sudo mount server_ip :/var/www/html /mnt/wordpress
+sudo mount {$nfs_server_ip}:/var/www/html /mnt/wordpress
 df -h
+sudo nano /etc/fstab
+ #Add this line
+    host_ip:/var/www/html    /mnt/wordpress  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0
